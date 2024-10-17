da113036b2d1229304cc29703dacc0b7e0e15255
JubayerAhmedSamrat <jasamrat@gmail.com>
2024-10-17 17:09:15 +0600

    1st

---
+++ script.js
@@ -0,0 +1,42 @@
+// function jubayer(){
+//     document.body.style.padding = "10px";
+//     document.button.style.backgroundColor = "black";
+//     document.button.style.color = "white";
+// }
+
+// addEventListener()
+
+// document.getElementById('btn').addEventListener('click', function jubayer(){
+//     console.log('jubayer');
+// })
+
+// const title = document.getElementById('btn');
+
+// title.addEventListener('click', function (){
+//     const para = document.getElementById('para');
+//     para.innerText = 'i am paragraph';
+//     console.log(para.innerText);
+// })
+ 
+// function jubayer(hi) {
+//     const input = document.getElementById('input');
+//     console.log(input.value);
+// }
+
+// function jubayer(){
+//     const para = document.getElementsByTagName('p');
+//     para.innerText = "mouse hover hocce";
+    
+// }
+
+// const para = document.getElementsByTagName('p');
+
+function jubayer(){
+    const para = document.getElementById('para');
+    para.innerText = "mouse hover hocce";
+    
+}
+function ridoy() {
+    const para = document.getElementById('para');
+    para.innerText = "mouse up hocce";
+}
